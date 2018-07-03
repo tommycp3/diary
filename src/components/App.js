bc05730252354a1b97f9ae6b11d6e0fe02ebef5f
Thomas Miller <thomas.miller@consensys.net>
2018-07-03 21:38:06 +1000

    added firebase and bootstrap form

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+
   render() {
     return (
-      <div className="App">
-        nothing
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-6 col-sm-offset-3">
+            <form>
+              <div className="form-group">
+                <input
+                  type="text"
+                  name="title"
+                  className="form-control no-border"
+                  placeholder="Title..."
+                  required
+                />
+              </div>
+
+              <div className="form-group">
+                <textarea
+                  type="text"
+                  name="body"
+                  className="form-control no-border"
+                  placeholder="Body..."
+                  required
+                />
+              </div>
+
+              <div className="form-group">
+                <button className="btn btn-primary col-sm-12">Save</button>
+
+
+              </div>
+
+
+            </form>
+          </div>
+
+
+        </div>
       </div>
     );
   }
--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase';
 
-  // Initialize Firebase
-  var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyDZHsdI5A5x_f75m4MgYZCO7WRft03IkNw",
     authDomain: "diary-e8b5f.firebaseapp.com",
     databaseURL: "https://diary-e8b5f.firebaseio.com",
     projectId: "diary-e8b5f",
     storageBucket: "diary-e8b5f.appspot.com",
     messagingSenderId: "321003594246"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
 export const database = firebase.database().ref('/notes');
