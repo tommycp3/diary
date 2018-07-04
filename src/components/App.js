d98ac9c9a71fe5ac62311df3c4f9692ff9eaa7bc
Thomas Miller <thomas.miller@consensys.net>
2018-07-04 13:47:24 +1000

    connected form to firebase note reference

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,47 @@
 import React, { Component } from 'react';
+import {database} from '../firebase';
+
+
 
 class App extends Component {
+
+
+  constructor(props) {
+    super(props);
+    // state
+    this.state = {
+      title: '',
+      body: ''
+    };
+    // bind
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+
+  }
+
+
+  // handle change
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  // handle submit
+  handleSubmit(e) {
+    e.preventDefault();
+    const note = {
+      title: this.state.title,
+      body: this.state.body
+    }
+    database.push(note);
+    console.log('submit');
+    this.setState({
+      title: '',
+      body: ''
+    });
+  }
+
 
 
   render() {
@@ -8,9 +49,11 @@ class App extends Component {
       <div className="container-fluid">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form>
+            <form onSubmit={this.handleSubmit}>
               <div className="form-group">
                 <input
+                  onChange={this.handleChange}
+                  value={this.state.title}
                   type="text"
                   name="title"
                   className="form-control no-border"
@@ -21,6 +64,8 @@ class App extends Component {
 
               <div className="form-group">
                 <textarea
+                  onChange={this.handleChange}
+                  value={this.state.body}
                   type="text"
                   name="body"
                   className="form-control no-border"
