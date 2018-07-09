2a368c9f0dad4fa9288432b66bb6f53aef7cf27e
Thomas Miller <thomas.miller@consensys.net>
2018-07-10 05:50:53 +1000

    created login component and added router

---
+++ src/components/Login.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    render() {
+        return (
+            <div className="continer-fluid">
+                <div className="row text-center">
+                    <div className="col-sm-12 jumbotron" style={{ marginTop: '-20px' }}>
+                        <h1>Login with your favourite <b>Social Network</b></h1>
+                    </div>
+
+                    <div className="col-sm-6">
+                        <button className="btn btn-danger btn-lg">Login with Google</button>
+                    </div>
+
+
+                    <div className="col-sm-6">
+                        <button className="bt btn-success btn-lg">Login with Twitter</button>
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -9,11 +9,41 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers/index';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import Login from './components/Login';
 
 // create redux store -> reducers -> 'actions - actionType' | applyMiddleware()
 // redux store requires reducers which requires actions
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+const Header = () => (
+    <nav className="navbar navbar-default">
+        <div className="container-fluid">
+            <div className="navbar-header">
+                <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span className="icon-bar" />
+                    <span className="icon-bar" />
+                    <span className="icon-bar" />
+                </button>
+
+                <Link className="navbar-brand" to="/" >DIARY2018</Link>
+
+            </div>
+
+            <div className="collapse navbar-collapse" id="myNavbar">
+                <ul className="nav navbar-nav navbar-right">
+                    <li><Link to="/login">Login</Link></li>
+                </ul>
+
+            </div>
+
+        </div>
+    </nav>
+
+);
+
+
 
 // provide the store to react
 
@@ -22,7 +52,16 @@ const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <BrowserRouter>
+            <div>
+                <Header />
+                <Switch>
+                    <Route path="/" component={App} exact={true} />
+                    <Route path="/login" component={Login} exact={true} />
+                </Switch>
+            </div>
+
+        </BrowserRouter>
     </Provider>, document.getElementById('root'));
 registerServiceWorker();
 
